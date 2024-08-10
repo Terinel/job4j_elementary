7aef1e3dd2b59fc48ee2bc96c7d1a77184dcbae8
Terinel <lex_neo@bk.ru>
2024-08-10 21:30:16 +0700

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -21,4 +21,20 @@ public class Calculator {
         size = size - 5;
 
     }
+
+    public class ArgumentsMethod {
+
+        public static void hello(String name, int age) {
+            System.out.println("Hello, " + name + ", age = " + age);
+        }
+
+        public static void main(String[] args) {
+            String name = "Job4j";
+            int age = 6;
+            ArgumentsMethod.hello(name, age);
+            ArgumentsMethod.hello(name, age);
+            ArgumentsMethod.hello(name, age);
+            ArgumentsMethod.hello(name, age);
+        }
+    }
 }
